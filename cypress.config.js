00690b96545bb95b456fc35623d7586776f3a569
imashaRan12 <162670381+imashaRan12@users.noreply.github.com>
2025-03-26 20:07:44 +0530

    feat: removed files

--- cypress.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  e2e: {
-    baseUrl: "http://localhost:8080",
-    integrationFolder: "cypress/integration",
-    viewportWidth: 1280,
-    viewportHeight: 720,
-  },
-};
--- tests/unit/example.test.js
+++
@@ -1,9 +0,0 @@
-// Import the module to be tested
-const { sum } = require("../../src/sum"); // Adjust the path to your module
-
-// Example test case
-describe("Sum Function", () => {
-  test("should add two numbers correctly", () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-});
